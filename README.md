8a433057fa20ec760a2a179dc2bf400e79bc59b7
unknown <alextello1@hotmail.com>
2020-10-31 13:04:39 -0600

    Funcionalidad para agregar y almacenartarjetas

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Proyecto que consta de tarjetas de preguntas y respuestas
+
+![captura](https://github.com/alextello/JS-Memory-cards/blob/main/img/1.png?raw=true)
--- script.js
+++ script.js
@@ -16,22 +16,24 @@ let currentActiveCard = 0;
 // Almacenar tarjetas DOM
 const cardsEl = [];
 
+// Almacenar datos de tarjetas
+const cardsData = getCardsData();
 
 // almacenar datos de tarjetas
-const cardsData = [
-    {
-        question: '¿Cómo debe empezar una variable?',
-        answer: 'Una letra, $ ó _'
-    },
-    {
-        question: '¿Qué es una variable?',
-        answer: 'Un contenedor para una porcion de datos'
-    },
-    {
-        question: 'Ejemplo de una variable Sensitive Case',
-        answer: 'estoEsUnaVariable'
-    },
-];
+// const cardsData = [
+//     {
+//         question: '¿Cómo debe empezar una variable?',
+//         answer: 'Una letra, $ ó _'
+//     },
+//     {
+//         question: '¿Qué es una variable?',
+//         answer: 'Un contenedor para una porcion de datos'
+//     },
+//     {
+//         question: 'Ejemplo de una variable Sensitive Case',
+//         answer: 'estoEsUnaVariable'
+//     },
+// ];
 
 // Creando las tarjetas
 function createCards() {
@@ -70,9 +72,23 @@ function updateCurrentText() {
     currentEl.innerText = `${currentActiveCard + 1} / ${cardsEl.length}`
 }
 
+// obtener tarjetas de localstorage
+function getCardsData() {
+    const cards = JSON.parse(localStorage.getItem('cards'));
+    return cards === null ? [] : cards;
+}
+
+// añadir tarjeta al localstorage
+function setCardsData(cards) {
+    localStorage.setItem('cards', JSON.stringify(cards));
+    window.location.reload();
+}
+
 createCards();
 
 // Event listeners
+
+// Botones de navegacion
 nextBtn.addEventListener('click', () => {
     cardsEl[currentActiveCard].className = 'card left';
     currentActiveCard += 1;
@@ -92,4 +108,26 @@ prevBtn.addEventListener('click', () => {
     }
     cardsEl[currentActiveCard].className = 'card active';
     updateCurrentText();
+});
+
+// mostrar contenedor para añadir
+showBtn.addEventListener('click', () => addContainer.classList.add('show'))
+
+// ocultar contenedor para añadir
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'))
+
+// añadir nueva tarjeta
+addCardBtn.addEventListener('click', () => {
+    const question = questionEl.value;
+    const answer = answerEl.value;
+
+    if (question.trim() && answer.trim()) {
+        const newCard = { question, answer };
+        createCard(newCard);
+        questionEl.value = '';
+        answerEl.value = '';
+        addContainer.classList.remove('show');
+        cardsData.push(newCard);
+        setCardsData(cardsData);
+    }
 });
